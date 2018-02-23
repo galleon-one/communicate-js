071cb7658531196000bf60e2551388053009bd98
upxsys <apps@upxsys.com>
2018-02-23 21:48:43 +0100

    Removed undeeded files

--- test/CommunicateSpec.js
+++ test/CommunicateSpec.js
@@ -7,6 +7,7 @@
  * LICENSE.txt file in the root directory of this source tree.
  */
 
-.footer {
+import { expect } from 'chai';
+import Communicate from '../src/Communicate';
 
-}
+// TODO: Write tests
